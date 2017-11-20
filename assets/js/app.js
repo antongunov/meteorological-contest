41d10bc41c642e0b4d736e1b2ec7b8e3b5a62095
Anton Gunov <anton@gunov.name>
2017-11-21 01:05:02 +0700

    Add app controller

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,60 @@
-(function(){
-  console.log('Loaded.');
-})();
+(function ({ BarChart, Controller, FilterForm }) {
+  const DEFAULT_CHART_TYPE = 'temperature';
+  const [MIN_YEAR, MAX_YEAR] = [1881, 2006];
+
+  // create controllers
+  const filter = new FilterForm('filter-form', {
+    defaultChartType: DEFAULT_CHART_TYPE,
+    minYear: MIN_YEAR,
+    maxYear: MAX_YEAR,
+  });
+
+  const app = new Controller('app', () => {
+    app.el.style.display = 'flex';
+
+    const canvas = document.getElementById('canvas');
+
+    const parentSize = canvas.parentNode.getBoundingClientRect();
+    canvas.width = parentSize.width;
+    canvas.height = parentSize.height;
+
+    app.barChart = new BarChart(canvas, {
+      barScale: 0.8,
+      colors: {
+        positiveValue: '#5799c7',
+        negativeValue: '#df5c5d',
+      },
+    });
+
+    app.drawBarChart(filter.values());
+
+    app.$on('filter-form:update', app.drawBarChart);
+  });
+
+  app.drawBarChart = (props) => {
+    const t0 = performance.now();
+
+    if (app.worker instanceof Worker) {
+      app.worker.terminate();
+      app.worker = null;
+    }
+
+    const worker = new Worker('assets/js/store/worker.js');
+
+    worker.postMessage(props);
+    app.worker = worker;
+
+    worker.onmessage = (event) => {
+      if (event.data.error) return console.error(event.data.error);
+      app.worker = null;
+
+      const values = Object.values(event.data);
+      app.barChart.draw(values);
+
+      console.log('dt= ', performance.now() - t0);
+    };
+
+    worker.onerror = event => console.error(event);
+  };
+
+})(window.App);
